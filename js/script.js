e896a3d92bde5f2562e189d1056a3ab6b3e8c391
Maria Soledade <maryaferreira94@gmail.com>
2021-04-19 19:42:27 -0300

    adicionando plugins de animação e slide

---
+++ js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: "destino",
+        nav: true,
+        auto: true,
+        time: 5000
+    });
+}
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
